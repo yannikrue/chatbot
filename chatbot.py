c22e78f49757bf49e52b773c123d297a9adacbca
Yannik <yannik.ruefenacht@stud.unibas.ch>
2022-08-31 17:12:04 +0200

    code clean up

--- chatbot.py
+++ chatbot.py
@@ -18,24 +18,24 @@ model = load_model('chatbot_model.h5')
 
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
+intents_json = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 
 
 def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-    return sentence_words
+    sen_words = nltk.word_tokenize(sentence)
+    sen_words = [lemmatizer.lemmatize(word.lower()) for word in sen_words]
+    return sen_words
 
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
+    sen_words = clean_up_sentence(sentence)
     # bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)
-    for s in sentence_words:
+    for s in sen_words:
         for i,w in enumerate(words):
             if w == s:
                 # assign 1 if current word is in the vocabulary position
@@ -44,13 +44,13 @@ def bow(sentence, words, show_details=True):
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
-def predict_class(sentence, model):
-    # filter out predictions below a threshold
+def predict(sentence, model):
+    # filter out low p cases
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.75
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
+    # sort by p
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
@@ -59,12 +59,12 @@ def predict_class(sentence, model):
         return_list.append({"intent": "unsure", "probability": "unsure"})
     return return_list
 
-def getResponse(ints, intents_json):
+def getResponse(ints, ints_json):
     tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
+    list_of_intents = ints_json['intents']
     for i in list_of_intents:
         if(i['tag']== tag):
-            result = random.choice(i['responses'])
+            res = random.choice(i['responses'])
             break
     if tag == "stocks":
         i = input("Do you want me to open Parqet, the portfolio visualizer? y/n \n")
@@ -83,12 +83,11 @@ def getResponse(ints, intents_json):
         i = input("Should I open the news app for more detailed information? y/n \n")
         if i == "y":
             webbrowser.open("https://www.srf.ch/")
-    return result
+    return res
 
 def chatbot_response(msg):
-    ints = predict_class(msg, model)
-    print(ints)
-    res = getResponse(ints, intents)
+    ints = predict(msg, model)
+    res = getResponse(ints, intents_json)
     return res
 
 
